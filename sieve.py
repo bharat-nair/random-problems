7044fbb6665cd697d4af426dfbae19162efdaabf
bharat.nair <bharatsnair@gmail.com>
2021-03-18 16:43:52 +0530

    sieve logic was wrong �‍♂️

--- sieve.py
+++ sieve.py
@@ -1,11 +1,26 @@
 # Sieve of Eratosthenes
+# create list of numbers - delete multiples of each number one by one - return remaining list
+# MAXIMUM SIEVE ACTION
 
 n = 120
-list_of_numbers = [i for i in range(2, n+1)]
+numbers = [i for i in range(2, n+1)]
+# [numbers.remove(m) for m in [i*j for i in numbers for j in numbers if i*j in numbers] if m in numbers]
+for i in numbers:
+    multiples = []
+    for j in numbers:
+        if i*j in numbers:
+            multiples.append(i*j)
+    for m in multiples:
+        numbers.remove(m)
+print(numbers)
 
-for number1 in list_of_numbers:
-    for number2 in list_of_numbers:
-        if number1 != number2:
-            if number2 % number1 == 0:
-                list_of_numbers.remove(number2)
-print(list_of_numbers)
+# Wrong logic below
+# n = 120
+# list_of_numbers = [i for i in range(2, n+1)]
+
+# for number1 in list_of_numbers:
+#     for number2 in list_of_numbers:
+#         if number1 != number2:
+#             if number2 % number1 == 0:
+#                 list_of_numbers.remove(number2)
+# print(list_of_numbers)
